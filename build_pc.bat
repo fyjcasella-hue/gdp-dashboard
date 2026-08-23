ccdcd1b850af35cbbd1c541200895e5ec68cfdb0
fyjcasella-hue <fyjcasella@gmail.com>
2026-08-23 14:38:10 -0300

    Build enhanced Giro Customs v2 executable

--- build_pc.bat
+++ build_pc.bat
@@ -1,7 +1,7 @@
 @echo off
 python -m pip install -r requirements.txt
 python -m pip install pyinstaller
-pyinstaller --noconfirm --onefile --windowed --name GiroCustoms giro_customs_pc.py
+pyinstaller --noconfirm --onefile --windowed --name GiroCustoms giro_customs_pc_v2.py
 echo.
 echo Ejecutable creado en dist\GiroCustoms.exe
 pause
