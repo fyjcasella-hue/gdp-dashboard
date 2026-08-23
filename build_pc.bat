3277dbff0b081d2d069ed1de766faf58c40d15f2
fyjcasella-hue <fyjcasella@gmail.com>
2026-08-23 14:35:51 -0300

    Add Windows build script

---
+++ build_pc.bat
@@ -0,0 +1,7 @@
+@echo off
+python -m pip install -r requirements.txt
+python -m pip install pyinstaller
+pyinstaller --noconfirm --onefile --windowed --name GiroCustoms giro_customs_pc.py
+echo.
+echo Ejecutable creado en dist\GiroCustoms.exe
+pause
